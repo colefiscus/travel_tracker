b3f396ee423713705bdc36fbc992e40504ec02a2
colefiscus <>
2021-01-14 16:48:40 -0700

    Add Destination Class and properties

---
+++ src/Destination.js
@@ -0,0 +1,10 @@
+export class Destination {
+  constructor() {
+    this.id = dest.id;
+    this.destination = dest.destination;
+    this.estLodgingCostPerDay = dest.estLodgingCostPerDay;
+    this.estFlightCostPerPers = dest.estFlightCostPerPers;
+    this.image = dest.image;
+    this.alt = dest.alt;
+  }
+}
